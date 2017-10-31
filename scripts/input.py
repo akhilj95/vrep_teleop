0558bdcd25323fabf42de602c78d751b313449e8
Akhil John <akhilmjohn@gmail.com>
2017-10-31 21:00:02 +0000

    Update input.py

--- scripts/input.py
+++ scripts/input.py
@@ -3,12 +3,39 @@
 import rospy
 from baxter_core_msgs.msg import DigitalIOState
 from sensor_msgs.msg import JointState
-from vrep_teleop import Joints
+from vrep_teleop.msg import Joints
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-class recordData:
+class RecordData:
+    def __init__(self):
+        self.left_button_pressed = 0
+        self.left_cuff_pressed = 0
+        self.right_cuff_pressed = 0
+        self.record = 0
+        self.start = 0
+        self.end = 0
+        self.n = 14
+        self.z = 0
+        self.currentPos = []
+        self.targetPos = []
+        self.newTargetPos = []
+        self.errorValue = []
+        self.targetVel = []
+        self.currentVel = []
+        self.newTargetVel = []
+        self.effort = []
+        self.ts = []
+        self.f1 = open("/home/akhil/box_30_15_15/turnDemoMaster100txt", "w+")
+        self.f2 = open("/home/akhil/box_30_15_15/turnDemoSlave100.txt", "w+")
+
+        rospy.Subscriber('robot/digital_io/left_lower_button/state', DigitalIOState, self.left_button)
+        rospy.Subscriber('robot/digital_io/left_lower_cuff/state', DigitalIOState, self.left_cuff)
+        rospy.Subscriber('robot/digital_io/right_lower_cuff/state', DigitalIOState, self.right_cuff)
+        rospy.Subscriber("/robot/joint_states", JointState, self.master_state)
+        rospy.Subscriber("/vrep/joints", Joints, self.slave_state)
+
     def left_button(self, data):
         if data.state == 1:
             self.left_button_pressed = 1
@@ -46,18 +73,24 @@ class recordData:
 
         if self.record == 1:
 
-            self.current.extend(data.currentPos)
-            self.target.extend(data.targetPos)
-            self.newTarget.extend(data.newTargetPos)
-            self.error.extend(data.errorValue)
-            self.inputVel.extend(data.vs)
-            self.compVel.extend(data.vel)
-            self.maxVel.extend(data.maxVelocity)
+            self.currentPos.extend(data.currentPos)
+            self.targetPos.extend(data.targetPos)
+            self.newTargetPos.extend(data.newTargetPos)
+            self.errorValue.extend(data.errorValue)
+            self.targetVel.extend(data.targetVel)
+            self.currentVel.extend(data.currentVel)
+            self.newTargetVel.extend(data.newTargetVel)
             self.effort.extend(data.effort)
             self.ts.extend(data.simTime)
             self.z = self.z + 1
 
-            rospy.loginfo("master %s", data.stamp)
+            rospy.loginfo("master %s", data.seq)
+            self.f2.write("seq ")
+            self.f2.write("%d " % data.seq)
+            self.f2.write("\n")
+            self.f2.write("timeStamp ")
+            self.f2.write("%f " % data.timeStamp)
+            self.f2.write("\n")
             self.f2.write("currentPos ")
             for i in range(0, self.n):
                 self.f2.write("%f " % (data.currentPos[i]))
@@ -74,17 +107,17 @@ class recordData:
             for i in range(0, self.n):
                 self.f2.write("%f " % (data.errorValue[i]))
             self.f2.write("\n")
-            self.f2.write("vs ")
+            self.f2.write("targetVel ")
             for i in range(0, self.n):
-                self.f2.write("%f " % (data.vs[i]))
+                self.f2.write("%f " % (data.targetVel[i]))
             self.f2.write("\n")
-            self.f2.write("vel ")
+            self.f2.write("currentVel ")
             for i in range(0, self.n):
-                self.f2.write("%f " % (data.vel[i]))
+                self.f2.write("%f " % (data.currentVel[i]))
             self.f2.write("\n")
-            self.f2.write("requiredVel ")
+            self.f2.write("newTargetVel ")
             for i in range(0, self.n):
-                self.f2.write("%f " % (data.maxVelocity[i]))
+                self.f2.write("%f " % (data.newTargetVel[i]))
             self.f2.write("\n")
             self.f2.write("effort ")
             for i in range(0, self.n):
@@ -106,56 +139,28 @@ class recordData:
         elif self.end == 1:
             self.f2.close()
 
-    def __init__(self):
-        self.left_button_pressed = 0
-        self.left_cuff_pressed = 0
-        self.right_cuff_pressed = 0
-        self.record = 0
-        self.start = 0
-        self.end = 0
-        self.n = 14
-        self.z = 0
-        self.current = []
-        self.target = []
-        self.newTarget = []
-        self.error = []
-        self.inputVel = []
-        self.compVel = []
-        self.maxVel = []
-        self.effort = []
-        self.ts = []
-        self.f1 = open("/home/akhiljohn/box_30_15_15/turnDemoMaster12txt", "w+")
-        self.f2 = open("/home/akhiljohn/box_30_15_15/turnDemoSlave12.txt", "w+")
-
-        rospy.Subscriber('robot/digital_io/left_lower_button/state', DigitalIOState, self.left_button)
-        rospy.Subscriber('robot/digital_io/left_lower_cuff/state', DigitalIOState, self.left_cuff)
-        rospy.Subscriber('robot/digital_io/right_lower_cuff/state', DigitalIOState, self.right_cuff)
-        rospy.Subscriber("/robot/joint_states", JointState, self.master_state)
-        rospy.Subscriber("/vrep/joints", Joints, self.slave_state)
-
 
 if __name__ == '__main__':
     rospy.init_node('trajectory_listener', anonymous=True)
     try:
-        startRecord = recordData()
+        startRecord = RecordData()
         while startRecord.end == 0:
             pass
 
-        jn = 0
         totalRead = startRecord.z
-        currentPos = np.array(startRecord.current).reshape((totalRead, startRecord.n))
-        targetPos = np.array(startRecord.target).reshape((totalRead, startRecord.n))
-        newTargetPos = np.array(startRecord.newTarget).reshape((totalRead, startRecord.n))
-        errorValue = np.array(startRecord.error).reshape((totalRead, startRecord.n))
-        vs = np.array(startRecord.inputVel).reshape((totalRead, startRecord.n))
-        vel = np.array(startRecord.compVel).reshape((totalRead, startRecord.n))
-        maxVelocity = np.array(startRecord.maxVel).reshape((totalRead, startRecord.n))
+        currentPos = np.array(startRecord.currentPos).reshape((totalRead, startRecord.n))
+        targetPos = np.array(startRecord.targetPos).reshape((totalRead, startRecord.n))
+        newTargetPos = np.array(startRecord.newTargetPos).reshape((totalRead, startRecord.n))
+        errorValue = np.array(startRecord.errorValue).reshape((totalRead, startRecord.n))
+        targetVel = np.array(startRecord.targetVel).reshape((totalRead, startRecord.n))
+        currentVel = np.array(startRecord.currentVel).reshape((totalRead, startRecord.n))
+        newTargetVel = np.array(startRecord.newTargetVel).reshape((totalRead, startRecord.n))
         effort = np.array(startRecord.effort).reshape((totalRead, startRecord.n))
         ts = np.array(startRecord.ts).reshape((totalRead, startRecord.n))
 
         for i in range(0, startRecord.n-14):
             plt.figure(i+1)
-            plt.figure(i+1).suptitle('Simulator:(moving)  points=5 order=3 lambda=10^-6  PGain=250 VGain=60')
+            plt.figure(i+1).suptitle('Joint'+str(i+1))
             plt.subplot(511)
             plt.plot(ts[:, i], targetPos[:, i], '.')
 
@@ -172,16 +177,16 @@ if __name__ == '__main__':
             plt.xlabel('errorValue')
 
             plt.subplot(515)
-            plt.plot(ts[:, i], vs[:, i], '.')
+            plt.plot(ts[:, i], targetVel[:, i], '.')
 
             plt.subplot(515)
-            plt.plot(ts[:, i], maxVelocity[:, i], 'x r')
+            plt.plot(ts[:, i], newTargetVel[:, i], 'x r')
 
             plt.subplot(515)
-            plt.plot(ts[:, i], vel[:, i], '. g')
+            plt.plot(ts[:, i], currentVel[:, i], '. g')
 
             plt.xlabel('masterVel(b), newTargetVel(r), slaveVel(g)')
-            plt.figure(i+1).savefig("sim_moving_5_3_10m6_250_60_jn"+str(i+1)+".png")
+            # plt.figure(i+1).savefig("jn"+str(i+1)+".png")
         plt.show()
 
     except rospy.ROSInterruptException:
